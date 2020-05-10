25ec9f3722b7c376b48e94433f1a6a89f35664d9
zhouzq254 <929562100@qq.com>
2020-05-10 19:49:08 +0800

    nacos配置动态生效

--- businsee-service/src/main/java/com/scnu/business/config/BusinessConfiguration.java
+++ businsee-service/src/main/java/com/scnu/business/config/BusinessConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDubbo(scanBasePackages="com.scnu.order.service")
+
+//enableRemoteSyncConfig 需要加上次配置，配置中心修改才能动态生效
 @EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "${nacos.config.server-address}", enableRemoteSyncConfig = "true")) // 激活 Nacos 配置
 @NacosPropertySource(dataId = "${nacos.config.data-id}", autoRefreshed = true)
 public class BusinessConfiguration {
